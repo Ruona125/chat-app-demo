f05788403a0202c9cbd83519630bf635d1b8a79e
Ruona125 <ogheneruonaagadagba4@gmail.com>
2024-02-17 22:15:05 +0100

    wrote the controller to get message

--- server/controllers/message.controller.js
+++ server/controllers/message.controller.js
@@ -16,20 +16,34 @@ export const sendMessage = async (req, res) => {
       });
     }
     const newMessage = new Message({
-        senderId,
-        receiverId, 
-        message
-    })
+      senderId,
+      receiverId,
+      message,
+    });
 
-    if(newMessage){
-        conversation.messages.push(newMessage._id)
+    if (newMessage) {
+      conversation.messages.push(newMessage._id);
     }
     // await conversation.save();
     // await newMessage.save();
-    await Promise.all([conversation.save(), newMessage.save()])
-    res.status(201).json(newMessage)
+    await Promise.all([conversation.save(), newMessage.save()]);
+    res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sending message", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessage = async (req, res) => {
+  try {
+    const { id: userToChat } = req.params;
+    const senderId = req.user._id;
+    const conversation = await Conversation.findOne({
+      participant: { $all: [senderId, userToChat] },
+    }).populate("messages")
+    res.status(200).json(conversation.messages)
+  } catch (error) {
+    console.log("Error in getMessage", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
--- server/routes/message.routes.js
+++ server/routes/message.routes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { sendMessage } from "../controllers/message.controller.js";
+import { sendMessage, getMessage } from "../controllers/message.controller.js";
 import protectRoute from "../middlware/protectRoutes.js";
 
 const router = express.Router();
 
 router.post("/send/:id", protectRoute, sendMessage)
+router.get("/:id", protectRoute, getMessage)
 
 export default router;
