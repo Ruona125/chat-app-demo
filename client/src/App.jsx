46c95eb0d90bfc7ff60cc4866dd5ab783193ea7c
Ruona125 <ogheneruonaagadagba4@gmail.com>
2024-02-19 01:13:19 +0100

    created the login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from "./Pages/login/Login";
+import { useAuthContext } from "./context/AuthContext";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { authUser } = useAuthContext();
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+      <Route path='/' element={authUser ? <Navigate to='/' /> : <Login />} />
+      </Routes>
+     
+    </div>
+  );
 }
 
-export default App
+export default App;
