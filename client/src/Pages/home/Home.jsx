b352aa150f2b995794e060094afd1ca2b9aad739
Ruona125 <ogheneruonaagadagba4@gmail.com>
2024-02-19 20:15:13 +0100

    added the home page

--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg 
+    overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter 
+    backdrop-blur-lg bg-opacity-0'>Home</div>
   )
 }
 
